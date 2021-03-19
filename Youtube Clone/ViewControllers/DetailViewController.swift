e169be95bcbe1670ddf53622623b2587cfea7250
Arzu95 <muhammadarzu3432@gmail.com>
2021-03-19 14:10:24 +0700

    Tambahan tadi

--- Youtube Clone/ViewControllers/DetailViewController.swift
+++ Youtube Clone/ViewControllers/DetailViewController.swift
@@ -16,33 +16,35 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var textView: UITextView!
     
-    var video: Video?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var video:Video?
+      
 
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+        override func viewDidLoad() {
+            super.viewDidLoad()
+
+            // Do any additional setup after loading the view.
+        }
+        
+      override func viewWillAppear(_ animated: Bool) {
         titleLabel.text = ""
         dateLabel.text = ""
         textView.text = ""
         
-        guard video != nil else{
-            return
+        guard video != nil else {
+          return
         }
+        
+        let embedUrlString = Constants.YT_EMBED_URL + video!.videoId
+        
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = df.string(from: video!.published)
+        
+        textView.text = video!.description
+      }
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
